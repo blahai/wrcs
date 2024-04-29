873d88707a065858c3dd1d9fb6f9662c73285492
blahai <github@blahai.gay>
2024-04-29 03:15:14 +0300

    Groundwork

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    // The command you want to run
+    let ydotool = "ydotool mousemove 50 50";
+    // Execute the command
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(ydotool)
+        .output()
+        .expect("Failed to execute command");
+
+    // Print the output of the command
+    if output.status.success() {
+        println!("Command executed successfully");
+        if let Ok(stdout) = String::from_utf8(output.stdout) {
+            print!("{}", stdout);
+        }
+    } else {
+        println!("Command failed to execute");
+        if let Ok(stderr) = String::from_utf8(output.stderr) {
+            eprint!("{}", stderr);
+        }
+    }
 }
